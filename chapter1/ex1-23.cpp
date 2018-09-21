2170aaa5d0496bd723d48248185217eb0c6a38b6
spencerpomme <spencerpomme@live.com>
2018-09-21 16:32:02 +0800

    ez

--- chapter1/ex1-23.cpp
+++ chapter1/ex1-23.cpp
@@ -2,5 +2,23 @@
 #include "Sales_item.h"
 
 int main() {
-    
+    Sales_item curr, last;
+    int count = 0;
+    std::cin >> last;
+    if (last) {
+        while (std::cin >> curr) {
+            if (curr.isbn() == last.isbn()) {
+                ++count;
+                last = curr;
+            } else {
+                std::cout << last.isbn() << ": " << count << std::endl;
+                count = 0;
+            }
+        }
+        std::cout << last.isbn() << ": " << std::endl;
+    } else {
+        std::cout << "No input data!" << std::endl;
+        return -1;
+    }
+    return 0;
 }
--- chapter1/ex1-24.cpp
+++ chapter1/ex1-24.cpp
@@ -2,5 +2,6 @@
 #include "Sales_item.h"
 
 int main() {
-    
+    // Just compile and run the program in ex1-23.cpp.
+    return 0;
 }
